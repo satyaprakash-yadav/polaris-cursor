3ebdc1cf1966262f079177e3b648c948a5e16da9
“satyaprakash-yadav” <satyay9798@gmail.com>
2026-09-12 23:49:59 +0530

    06: error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -39,7 +39,24 @@ export const demoGenerate = inngest.createFunction(
       return await generateText({
         model: google('gemini-3.5-flash'),
         prompt: finalPrompt,
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       });;
     });
   }
+);
+
+export const demoError = inngest.createFunction(
+  {
+    id: "demo-error",
+    triggers: { event: "demo/error" }
+  },
+  async ({ step }) => {
+    await step.run("fail", async () => {
+      throw new Error("Inngest error: Background job failed!");
+    });
+  },
 );
